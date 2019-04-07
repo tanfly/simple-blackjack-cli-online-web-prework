34ad1e3abaeada266d47ad6af68b72f2bbe9a842
Stella McEwen <mcewen.stella@gmail.com>
2019-04-07 23:21:01 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -50,9 +50,9 @@ end
 
 def runner
   welcome
-  card_total = initial_round
-  until card_total > 21
-  card_total = hit?(total)
+  total = initial_round
+  until total > 21
+  total = hit?(total)
 end
 end_game
 end
