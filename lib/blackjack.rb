7f225a3ea2989e6f0d52c72f631304d780ab8d58
Stella McEwen <mcewen.stella@gmail.com>
2019-04-07 22:57:56 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -31,10 +31,10 @@ end
 
 def hit?(total)
   prompt_user
-  get_user_input == user_input
-  if user_input == 'h'
+  get_user_input
+  if get_user_input == 'h'
    total += deal_card
-  elsif user_input == 's'
+  elsif get_user_input == 's'
   total
 end
 end
