5fff943cb69eab032a92894e06ba0e92a66ccd17
Stella McEwen <mcewen.stella@gmail.com>
2019-04-07 21:53:53 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,11 +7,11 @@ def deal_card
 end
 
 def display_card_total(total)
-  return total
+
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
